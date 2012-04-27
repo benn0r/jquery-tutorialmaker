a6a2da0f431db0c6c1e00b4bad880ebaed19fe83
benn0r <admin@benn0r.ch>
2012-04-27 13:19:42 +0200

    brackets and popover

--- js/jquery.tutorialmaker.js
+++ js/jquery.tutorialmaker.js
@@ -5,37 +5,94 @@
 	{
 		var elem = step.elem;
 		
+		if ($('.tm-bracket').size() > 0) {
+			// create new bracket element
+			console.log($('.tm-bracket').data('popover'));
+			$($('.tm-bracket').data('popover').$tip).remove();
+			$('.tm-bracket').remove();
+		}
+		$('body').append('<div class="tm-bracket"></div>');
+		
 		// overlay top
 		$('.tm-otop').animate({
-			height: elem.offset().top
+			height: elem.offset().top - 5
 		}, options.speed);
 		
 		// overlay bottom
 		$('.tm-obottom').animate({
-			height: $(document).height() - 
-				elem.offset().top - elem.outerHeight()
+			height: $(window).height() - 
+				elem.offset().top - elem.outerHeight() - 5
 		}, options.speed);
 		
 		// overlay left
 		$('.tm-oleft').animate({
-			top: elem.offset().top,
-			bottom: $(document).height() - 
-				elem.offset().top - elem.outerHeight(),
-			width: elem.offset().left
+			top: elem.offset().top - 5,
+			bottom: $(window).height() - 
+				elem.offset().top - elem.outerHeight() - 5,
+			width: elem.offset().left - 5
 		}, options.speed);
 		
 		// overlay right
 		$('.tm-oright').animate({
-			top: elem.offset().top,
-			bottom: $(document).height() - 
-				elem.offset().top - elem.outerHeight(),
+			top: elem.offset().top - 5,
+			bottom: $(window).height() - 
+				elem.offset().top - elem.outerHeight() - 5,
 			width: $(document).width() - 
-				elem.offset().left - elem.outerWidth()
+				elem.offset().left - elem.outerWidth() - 5
 		}, options.speed);
 		
 		setTimeout(function() {
-			options.next();
-		}, 1000);
+			// display bracket
+			switch (step.position) {
+				case 'top':
+					$('.tm-bracket').addClass('tm-btop');
+					$('.tm-bracket').css('top', elem.offset().top - 15);
+					$('.tm-bracket').css('left', elem.offset().left - 15);
+					$('.tm-bracket').width(elem.outerWidth() + 20);
+					break;
+				case 'right':
+					$('.tm-bracket').addClass('tm-bright');
+					$('.tm-bracket').css('top', elem.offset().top - 15);
+					$('.tm-bracket').css('left', elem.offset().left + elem.outerWidth());
+					$('.tm-bracket').height(elem.outerHeight() + 20);
+					break;
+				case 'bottom':
+					$('.tm-bracket').addClass('tm-bbottom');
+					$('.tm-bracket').css('top', elem.offset().top + elem.outerHeight());
+					$('.tm-bracket').css('left', elem.offset().left - 15);
+					$('.tm-bracket').width(elem.outerWidth() + 20);
+					break;
+				case 'left':
+					$('.tm-bracket').addClass('tm-bleft');
+					$('.tm-bracket').css('top', elem.offset().top - 15);
+					$('.tm-bracket').css('left', elem.offset().left - 15);
+					$('.tm-bracket').height(elem.outerHeight() + 20);
+					break;
+			}
+			
+			var nextbtn = $('<a href="" class="btn btn-success btn-mini pull-right">' + 
+				step.button + '</a>');
+			
+			nextbtn.data('options', options);
+			nextbtn.click(function() {
+				$(this).data('options').next();
+				return false;
+			});
+			
+			$('.tm-bracket').popover({
+				title: step.title,
+				content: step.body,
+				placement: step.position,
+				trigger: 'manual'
+			});
+			$('.tm-bracket').popover('show');
+			
+			if (step.button) {
+				$($('.tm-bracket').data('popover').$tip)
+					.find('.popover-title')
+					.append(nextbtn);
+			}
+		}, options.speed);
 	};
 
 	$.tm = function(options)
@@ -43,13 +100,16 @@
 		var settings = $.extend({
 			steps: [],
 			speed: 200,
-			step: 0,
+			step: -1,
 		}, options);
 		
+		// go to next step
 		settings.next = function() {
 			settings.step++;
-			settings.steps[settings.step].elem.tm(settings, 
-				settings.steps[settings.step]);
+			if (settings.steps[settings.step]) {
+				settings.steps[settings.step].elem.tm(settings, 
+						settings.steps[settings.step]);
+			}
 		};
 		
 		$('body').append('<div class="tm-overlay tm-otop"></div>');
@@ -57,7 +117,9 @@
 		$('body').append('<div class="tm-overlay tm-obottom"></div>');
 		$('body').append('<div class="tm-overlay tm-oleft"></div>');
 		
-		settings.steps[0].elem.tm(settings, settings.steps[0]);
+//		settings.steps[0].elem.tm(settings, settings.steps[0]);
+		
+		return settings;
 	};
 	
 })(jQuery);
